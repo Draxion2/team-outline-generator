265773d485e439d9edc8d32361b0fe43f4173332
= <holdanmcmullen@gmail.com>
2022-04-04 01:49:37 -0600

    fixed bugs

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,10 @@
 const Employee = require("../lib/Employee");
+
+// test employee object
+test("creates the employee object", () => {
+    const newEmployee = new Employee("James", "james@gmail.com", 1);
+
+    expect(newEmployee.name).toBe("James");
+    expect(newEmployee.getEmail()).toEqual(newEmployee.email);
+    expect(newEmployee.getId()).toEqual(newEmployee.id);
+});
